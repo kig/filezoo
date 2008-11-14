f69d1352eff892c605c32c546f0b5c9a5eb843a4
Ilmari Heikkinen <ilmari.heikkinen@gmail.com>
2008-11-14 14:15:49 +0200

    somewhat working traversal server

---
+++ dirstats_traversal.cs
@@ -0,0 +1,205 @@
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.IO;
+using Mono.Unix;
+using Cairo;
+
+public class DirStats
+{
+  public static Color directoryColor = new Color (0,0,1);
+  public static Color blockDeviceColor = new Color (0.75,0.5,0);
+  public static Color characterDeviceColor = new Color (0.75,0.5,0);
+  public static Color fifoColor = new Color (0.75,0,0.82);
+  public static Color socketColor = new Color (0.75,0,0);
+  public static Color symlinkColor = new Color (0,0.75,0.93);
+  public static Color executableColor = new Color (0,0.75,0);
+  public static Color fileColor = new Color (0,0,0);
+
+  public double Scale;
+  public double Zoom;
+  public double Height;
+  public string Name;
+  public string FullName;
+  public double Length;
+  public string Suffix;
+  public bool IsDirectory = false;
+  public UnixFileSystemInfo Info;
+  public bool TraversalCancelled = false;
+
+  public double BoxWidth = 100.0;
+
+  public double MinFontSize = 0.5;
+  public double MaxFontSize = 16.0;
+
+  private bool recursiveSizeComputed = false;
+
+  Traversal TraversalServer;
+  Traversal.DirectoryEntry TraversalInfo;
+
+  public DirStats (Traversal ts, UnixFileSystemInfo f)
+  {
+    TraversalServer = ts;
+    Scale = Zoom = Height = 0.0;
+    Info = f;
+    Name = f.Name;
+    FullName = f.FullName;
+    Length = f.Length;
+    try { IsDirectory = Info.IsDirectory; } catch (System.InvalidOperationException) {}
+    string[] split = Name.Split('.');
+    Suffix = (Name[0] == '.') ? "" : split[split.Length-1];
+  }
+
+  public double GetScaledHeight ()
+  {
+    return Height * Scale * Zoom * 1000;
+  }
+
+  public string GetSubTitle () { return GetSubTitle (true); }
+
+  public string GetSubTitle ( bool complexSubTitle )
+  {
+    if (IsDirectory) {
+      string extras = "";
+      extras += String.Format("{0} files", (complexSubTitle ? GetRecursiveCount() : 0).ToString("N0"));
+      extras += String.Format(", {0} total", FormatSize(complexSubTitle ? GetRecursiveSize() : 0));
+      return extras;
+    } else {
+      return String.Format("{0}", FormatSize(Length));
+    }
+  }
+
+  public static string FormatSize (double sz)
+  {
+    string suffix = "";
+    if (sz >= 1e9) {
+      suffix = "G";
+      sz /= 1e9;
+    } else if (sz >= 1e6) {
+      suffix = "M";
+      sz /= 1e6;
+    } else if (sz >= 1e3) {
+      suffix = "k";
+      sz /= 1e3;
+    } else {
+      return String.Format("{0} B", sz.ToString("N0"));
+    }
+    return String.Format("{0} {1}B", sz.ToString("N1"), suffix);
+  }
+
+  public void Draw (Context cr) { Draw (cr, true); }
+
+  public void Draw (Context cr, bool complexSubTitle)
+  {
+    double h = GetScaledHeight ();
+    cr.Save ();
+      cr.Rectangle (0.0, 0.0, BoxWidth, h*0.98);
+      Color c = GetColor (Info.FileType, Info.FileAccessPermissions);
+      cr.Color = c;
+      cr.Fill ();
+      double fs = GetFontSize(h);
+      cr.MoveTo (BoxWidth * 1.1, 0.0);
+      cr.RelMoveTo(0, h*0.5 - fs);
+      if (fs > 4) {
+        Helpers.DrawText (cr, fs, Name);
+        cr.RelMoveTo(0, fs*0.35);
+        Helpers.DrawText (cr, fs * 0.7, "  " + GetSubTitle (complexSubTitle));
+      } else if (fs > 1) {
+        Helpers.DrawText (cr, fs, Name + "  " + GetSubTitle (complexSubTitle));
+      } else {
+        cr.Rectangle (BoxWidth * 1.1, h*0.5 - fs, fs / 2 * (Name.Length+15), fs/3);
+        cr.Fill ();
+      }
+    cr.Restore ();
+  }
+
+  double GetFontSize(double h) {
+    return Math.Max(MinFontSize, QuantizeFontSize(Math.Min(MaxFontSize, 0.5 * h)));
+  }
+
+  double QuantizeFontSize (double fs) {
+    return Math.Floor(fs);
+  }
+
+  public bool[] Click (Context cr, double totalSize, double x, double y)
+  {
+
+    double h = GetScaledHeight ();
+    double fs = GetFontSize(h);
+    bool[] retval = {false, false};
+    if (fs < 4) return retval;
+    double advance = 0.0;
+    cr.Save ();
+      cr.NewPath ();
+      advance += Helpers.GetTextExtents (cr, fs, Name).XAdvance;
+      advance += Helpers.GetTextExtents (cr, fs*0.7, "  " + GetSubTitle ()).XAdvance;
+      cr.Rectangle (0.0, 0.0, BoxWidth * 1.1 + advance, h);
+      cr.IdentityMatrix ();
+      retval[0] = cr.InFill(x,y);
+    cr.Restore ();
+    if (retval[0])
+      retval[1] = OpenFile ();
+    return retval;
+  }
+
+  public string GetFullPath ()
+  {
+    return FullName;
+  }
+
+  public bool TraversalInProgress {
+    get {
+      if (TraversalInfo == null) return false;
+      return !TraversalInfo.Complete;
+    }
+  }
+
+  public double GetRecursiveSize ()
+  {
+    if (!recursiveSizeComputed) {
+      recursiveSizeComputed = true;
+      if (IsDirectory) {
+        TraversalInfo = TraversalServer.RequestInfo(GetFullPath());
+      } else {
+        TraversalInfo = new Traversal.DirectoryEntry(GetFullPath());
+        TraversalInfo.TotalSize = Length;
+        TraversalInfo.TotalCount = 1.0;
+        TraversalInfo.Complete = true;
+      }
+    }
+    return TraversalInfo.TotalSize;
+  }
+
+  public double GetRecursiveCount ()
+  {
+    if (!recursiveSizeComputed)
+      GetRecursiveSize ();
+    return TraversalInfo.TotalCount;
+  }
+
+  bool OpenFile ()
+  {
+    if (IsDirectory) {
+      return true;
+    } else {
+      Process proc = Process.Start ("gnome-open", GetFullPath ());
+      proc.WaitForExit ();
+      return false;
+    }
+  }
+
+  static Color GetColor (FileTypes filetype, FileAccessPermissions perm)
+  {
+    switch (filetype) {
+      case FileTypes.Directory: return directoryColor;
+      case FileTypes.BlockDevice: return blockDeviceColor;
+      case FileTypes.CharacterDevice: return characterDeviceColor;
+      case FileTypes.Fifo: return fifoColor;
+      case FileTypes.Socket: return socketColor;
+      case FileTypes.SymbolicLink: return symlinkColor;
+    }
+    if ((perm & FileAccessPermissions.UserExecute) != 0)
+      return executableColor;
+    return fileColor;
+  }
+}
--- traversal.cs
+++ traversal.cs
@@ -1,25 +1,144 @@
 using System;
 using System.Diagnostics;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading;
 using System.IO;
 using Mono.Unix;
 
 public class Traversal {
 
-  class DirectoryEntry {
+  Thread Worker;
+  Queue<DirectoryEntry> TraversalRequests;
+  Dictionary<string, DirectoryEntry> Entries;
+
+  public Traversal () {
+    TraversalRequests = new Queue<DirectoryEntry> ();
+    Entries = new Dictionary<string, DirectoryEntry> ();
+    ThreadStart w = new ThreadStart (ProcessQueue);
+    Worker = new Thread (w);
+    Worker.IsBackground = true;
+    Worker.Priority = ThreadPriority.BelowNormal;
+    Worker.Start ();
+  }
+
+  void ProcessQueue () {
+    while (true) {
+      while (TraversalRequests.Count > 0) {
+        DirectoryEntry d = TraversalRequests.Dequeue ();
+        Console.WriteLine("Processing {0}", d.Path);
+        ArrayList sd = d.Directories;
+        d.TotalSize = d.FilesizeSum;
+        d.TotalCount = d.Entries.Length;
+        bool allComplete = true;
+        if (sd.Count > 0) {
+          foreach (UnixFileSystemInfo s in sd) {
+            DirectoryEntry se = RequestInfo(s.FullName);
+            if (se.Complete) {
+              d.TotalSize += se.TotalSize;
+              d.TotalCount += se.TotalCount;
+            } else {
+              allComplete = false;
+            }
+          }
+        }
+        PropagateTotalSize(d);
+        if (allComplete) SetComplete(d);
+      }
+    }
+  }
+
+  void PropagateTotalSize (DirectoryEntry d) {
+    foreach (string a in d.Ancestors)
+      if (Entries.ContainsKey(a)) {
+        Entries[a].TotalSize += d.TotalSize;
+        Entries[a].TotalCount += d.TotalCount;
+      }
+  }
+
+  void SetComplete (DirectoryEntry d) {
+    foreach (string a in d.Ancestors) {
+      if (Entries.ContainsKey(a)) {
+        DirectoryEntry ae = Entries[a];
+        if (ae.Complete) continue;
+        ArrayList sd = ae.Directories;
+        bool allComplete = true;
+        foreach (UnixFileSystemInfo s in sd) {
+          if (!RequestInfo(s.FullName).Complete) {
+            allComplete = false;
+          }
+        }
+        ae.Complete = allComplete;
+      }
+    }
+  }
+
+  public DirectoryEntry RequestInfo (string path) {
+    Console.WriteLine("Requesting {0}", path);
+    if (!Entries.ContainsKey(path)) {
+      Console.WriteLine("Queuing {0}", path);
+      DirectoryEntry d = new DirectoryEntry (path);
+      Entries.Add(path, d);
+      TraversalRequests.Enqueue (d);
+    }
+    return Entries[path];
+  }
+
+  public class DirectoryEntry {
     public bool Complete;
     public string Path;
     public double TotalSize;
+    public double TotalCount;
+    public ArrayList Ancestors;
 
     public DirectoryEntry (string path) {
       Path = path;
       Complete = false;
       TotalSize = 0.0;
+      TotalCount = 0.0;
+      Ancestors = new ArrayList ();
+      string[] segments = path.Split('/');
+      string build = "";
+      foreach (string s in segments) {
+        build = build + "/" + s;
+        Ancestors.Add(build);
+      }
     }
 
-    public void SetTotalSize (double sz) { TotalSize = sz; }
-    public void SetComplete (bool c) { Complete = c; }
+
+    public double FilesizeSum {
+      get {
+        double sum = 0.0;
+        foreach (UnixFileSystemInfo f in Files)
+          sum += (double)f.Length;
+        return sum;
+      }
+    }
+
+    public UnixFileSystemInfo[] Entries {
+      get {
+        UnixDirectoryInfo di = new UnixDirectoryInfo (Path);
+        try { return di.GetFileSystemEntries (); }
+        catch (System.UnauthorizedAccessException) { return new UnixFileSystemInfo[0]; }
+      }
+    }
+    public ArrayList Directories {
+      get {
+        ArrayList al = new ArrayList ();
+        foreach (UnixFileSystemInfo e in Entries)
+          if (e.IsDirectory) al.Add(e);
+        return al;
+      }
+    }
+    public ArrayList Files {
+      get {
+        ArrayList al = new ArrayList ();
+        foreach (UnixFileSystemInfo e in Entries)
+          if (!e.IsDirectory) al.Add(e);
+        return al;
+      }
+    }
   }
 
 }
+
